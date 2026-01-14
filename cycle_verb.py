7300f6627bee7c8a4a55d29ac60ad647dfc8b63b
chrahe <christian.rahe@uni-hamburg.de>
2026-01-14 20:08:32 +0100

    added word cycle effect

---
+++ cycle_verb.py
@@ -0,0 +1,69 @@
+cycle_verb_style = """
+<style>
+    #cycle-verb::before {
+        display: inline-block;
+        width: 5ch;
+        text-align: center;
+        animation: word-cycle 21s infinite, fade-cycle 7s infinite;
+
+        @starting-style {
+            opacity: 1;
+            transform: translateY(0);
+        }
+    }
+
+    @keyframes word-cycle {
+        0% {
+            content: "like";
+            font-weight: normal;
+        }
+
+        33% {
+            content: "like";
+        }
+
+        34% {
+            content: "adore";
+        }
+
+        66% {
+            content: "adore";
+            font-weight: normal;
+        }
+
+        67% {
+            content: "love";
+            font-weight: bold;
+        }
+
+        100% {
+            content: "love";
+            font-weight: bold;
+        }
+    }
+
+    @keyframes fade-cycle {
+        0% {
+            opacity: 0;
+            transform: translateY(-10%);
+        }
+
+        5% {
+            opacity: 1;
+            transform: translateY(0);
+        }
+
+        95% {
+            opacity: 1;
+            transform: translateY(0);
+        }
+
+        100% {
+            opacity: 0;
+            transform: translateY(10%);
+        }
+    }
+</style>
+"""
+
+cycle_verb_element = """<span id="cycle-verb"></span>"""
--- script.py
+++ script.py
@@ -4,12 +4,13 @@ import time
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
+from cycle_verb import cycle_verb_style, cycle_verb_element
 from illions import format_illions
 
 st.title("So much")
 
-tz_hers=ZoneInfo("Asia/Kolkata")
-tz_his=ZoneInfo("Europe/Berlin")
+tz_hers = ZoneInfo("Asia/Kolkata")
+tz_his = ZoneInfo("Europe/Berlin")
 
 time_between = (datetime.now(tz=tz_hers) - datetime(2025, 11, 21, 2, 30, tzinfo=tz_hers))
 time_since = time_between.total_seconds() / 3600
@@ -17,8 +18,10 @@ days_since = time_between.days
 
 message = "It's been a minute, my heart. Well, more than a minute. Just around __{days}__ days.".format(days=days_since)
 
-really_scalar = 3 * (2**(time_since / 24))
+really_scalar = 3 * (2 ** (time_since / 24))
 really_formatted = format_illions(really_scalar)
+
+
 # how_much = really_scalar * (1 + st.session_state.get("seasoning_input", 0) / 100)
 
 def stream_message():
@@ -26,13 +29,15 @@ def stream_message():
         yield char
         time.sleep(0.03)
 
-st.write(f"I really<sup>({really_formatted})</sup> like you.", unsafe_allow_html=True)
+
+st.html(cycle_verb_style)
+st.html(f"""I really<sup>({really_formatted})</sup> {cycle_verb_element} you.""")
 if st.button("Aren't you a curious thing?"):
     st.write_stream(stream_message)
     with st.popover("Still curious?"):
         st.write(f"In terms of hours, the series has been on for {time_since:.2f}!")
         with st.popover("More?"):
-            st.write(f"{time_since*60:.0f} minutes and counting <3")
+            st.write(f"{time_since * 60:.0f} minutes and counting <3")
             with st.popover("Go ahead, let me disregard all good design practices."):
                 st.write(f"That's {time_between.total_seconds():.2f} seconds of absolute *aboration*!")
     st.balloons()
